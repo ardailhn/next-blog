a0b486cd431716222be23f94ba7d6401f56ac942
Arda İlhan <arda.ilhan@kayraexport.com>
2023-09-25 12:13:28 +0300

    blog page seo optimization

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,6 +3,7 @@ import styles from './singlePage.module.css'
 import Menu from '../../components/menu/Menu'
 import Image from 'next/image'
 import Comments from '../../components/comments/Comments'
+import { Metadata } from 'next'
 
 const getData = async ({ slug }) => {
     const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
@@ -16,11 +17,63 @@ const getData = async ({ slug }) => {
     return res.json();
 }
 
+type Props = {
+    params: { slug: string }
+}
+
+export async function generateMetadata(
+    { params }: Props
+): Promise<Metadata> {
+    const { slug } = params;
+    const post = await getData({ slug });
+
+    return {
+        title: post.title,
+        description: post.desc.substring(0, 160),
+        openGraph: {
+            images: [post.img],
+        },
+        authors: {
+            name: post.user.name,
+            url: post.user.image,
+        },
+        other: {
+            datePublished: post.createdAt,
+            headline: post.title,
+            image: [post.img],
+        },
+        publisher: "arda ilhan"
+    }
+}
+
 const SinglePage = async ({ params }) => {
     const { slug } = params;
     const post = await getData({ slug });
+
+    const jsonLd = {
+        '@context': 'https://schema.org',
+        '@type': 'NewsArticle',
+        headline: post.title,
+        image: [post.img],
+        datePublished: post.createdAt,
+        author: [{
+            "@type": "Person",
+            "name": post.user.name,
+            "url": post.user.image
+        }],
+        publisher: [{
+            "name": "arda ilhan"
+        }],
+        name: post.name,
+        description: post.desc,
+    }
+
     return (
-        <div className={styles.container}>
+        <section className={styles.container}>
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+            />
             <div className={styles.infoContainer}>
                 <div className={styles.textContainer}>
                     <h1 className={styles.title}>{post.title}</h1>
@@ -47,7 +100,7 @@ const SinglePage = async ({ params }) => {
                 </div>
                 <Menu />
             </div>
-        </div>
+        </section>
     )
 }
 
