1e4ea4ecb27f162f0e1ecc54e062144fd5273433
Arda İlhan <arda.ilhan@kayraexport.com>
2023-09-25 16:48:51 +0300

    sitemap bug fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,29 +2,39 @@ import { getAllCategorySlugs } from '@/services/category.service';
 import { getAllPostSlugs } from '@/services/post.service'
 import { MetadataRoute } from 'next'
 
+interface ISlug {
+    slug: string
+}
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-
-    const postSlugs = await getAllPostSlugs();
-
-    const categorySlugs = await getAllCategorySlugs();
+    let postSlugs: ISlug[] = []
+    let categorySlugs: ISlug[] = []
+    try {
+        [postSlugs, categorySlugs] = await Promise.all([
+            getAllPostSlugs(),
+            getAllCategorySlugs()
+        ])
+    } catch (error) {
+        console.error('error: ', error);
+    }
 
     return [
         {
-            url: process.env.NEXT_PUBLIC_APP_URL,
-            lastModified: new Date(),
+            url: process.env.NEXT_PUBLIC_APP_URL!,
+            lastModified: new Date().toISOString(),
             changeFrequency: 'daily',
             priority: 1,
         },
         ...postSlugs.map((slug: { slug: string }) => ({
-            url: process.env.NEXT_PUBLIC_APP_URL + slug.slug,
-            lastModified: new Date(),
-            changeFrequency: 'daily',
+            url: process.env.NEXT_PUBLIC_APP_URL! + slug.slug,
+            lastModified: new Date().toISOString(),
+            changeFrequency: 'daily' as const,
             priority: 0.7,
         })),
         ...categorySlugs.map((slug: { slug: string }) => ({
-            url: process.env.NEXT_PUBLIC_APP_URL + `blog?category=${slug.slug}`,
-            lastModified: new Date(),
-            changeFrequency: 'daily',
+            url: process.env.NEXT_PUBLIC_APP_URL! + `blog?category=${slug.slug}`,
+            lastModified: new Date().toISOString(),
+            changeFrequency: 'daily' as const,
             priority: 0.7,
         })),
     ]
